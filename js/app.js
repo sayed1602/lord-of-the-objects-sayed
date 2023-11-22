1863d9ec87a958e277405a69bae9498f8a4eecab
Sayed <sayedfkabir@gmail.com>
2023-11-21 20:11:49 -0700

    Chapter 3 complete - Made the ring and gave it to Frodo

--- js/app.js
+++ js/app.js
@@ -96,11 +96,12 @@ function keepItSecretKeepItSafe() {
   console.log("3: keepItSecretKeepItSafe");
 
   // create a div with an id of `'the-ring'`
-
+  const theRing = document.createElement("div");
+  theRing.setAttribute("id", "the-ring");
   // give the div a class of `'magic-imbued-jewelry'`
-
+  theRing.classList.add("magic-imbued-jewelry");
   // add the ring as a child of `Frodo`
-
+  document.querySelectorAll(".hobbit")[0].appendChild(theRing);
 }
 
 // COMMIT YOUR WORK
